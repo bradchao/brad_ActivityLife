ac373d427a290989991e800169091d190c0cbf6f
Brad <brad@brad.tw>
2016-09-24 11:19:59 +0800

    v6

--- app/src/main/java/brad/tw/activitylife/MainActivity.java
+++ app/src/main/java/brad/tw/activitylife/MainActivity.java
@@ -61,24 +61,27 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
         Log.d("brad", "onStop");
     }
-
     public void test1(View v){
-        // FIXME - mmm kjjfg jkjjdsfjj dfg
         Intent it = new Intent(this, Page2Activity.class);
         it.putExtra("username", "brad");
         it.putExtra("stage", 4);
         it.putExtra("sound", false);
         startActivity(it);
     }
-
-    public void test2(View v){
-        // TODO - jj jtjj ejjt ert
-        finish();
-    }
-
+    public void test2(View v){finish();}
     public void test3(View v){
-        // xxxxxx
-        setContentView(R.layout.activity_page2);
+        Intent it = new Intent(this, Page2Activity.class);
+        it.putExtra("username", "brad");
+        it.putExtra("stage", 4);
+        it.putExtra("sound", false);
+
+        //startActivity(it);
+        startActivityForResult(it, 123);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("brad", "onActivityResult");
+    }
 }

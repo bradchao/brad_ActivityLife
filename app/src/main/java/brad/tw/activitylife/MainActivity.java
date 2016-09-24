461c5c74a697d949b678ad454e39610e918c925d
Brad <brad@brad.tw>
2016-09-24 09:51:41 +0800

    v2

--- app/src/main/java/brad/tw/activitylife/MainActivity.java
+++ app/src/main/java/brad/tw/activitylife/MainActivity.java
@@ -2,6 +2,7 @@ package brad.tw.activitylife;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,36 +10,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       Log.d("brad", "onCreate");
+
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
+        Log.d("brad", "onRestart");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Log.d("brad", "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d("brad", "onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Log.d("brad", "onPause");
     }
 
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.d("brad", "onDestroy");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        Log.d("brad", "onStop");
     }
 }
